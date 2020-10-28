6210f3f85228f854d912896cac30f78d41b96238
nedoletoff <nedoletoff@yandex.ru>
2020-10-29 00:00:46 +0300

    add sort

--- c16.c
+++ c16.c
@@ -11,6 +11,8 @@ int main()
 	};
 
 	struct word words[N/2] = {0}; 
+	struct word temp = {0};
+	int temp_count = 0;
 	char str[N] = {'\0'};
 	int num[N] = {0};
 	int count = 0;
@@ -18,7 +20,7 @@ int main()
 	printf("Type string: ");
 	str[0] = getchar();
 	num[0] = str[0];
-	for (int i = 1; i < N - 1; ++i)
+	for (int i = 1; i < N - 1; ++i)		//read string
 	{
 		str[i] = getchar();
 		if ( str[i-1] == ' ' || str[i] == ' ') 
@@ -30,28 +32,42 @@ int main()
 		}
 	}
 
-		for (int i = 'A'; i <= 'z'; ++i)
+	for (int i = 'A'; i <= 'z'; ++i)	// add weight and count words
+	for (int j = 0; j < N; ++j) 
 	{
-		for (int j = 0; j < N; ++j)
+		if (i == num[j])
 		{
-			if (i == num[j])
+			words[count].first = j;
+			while (num[j] != ' ')
 			{
-				words[count].first = j;
-				while (num[j] != ' ')
-				{
-				       words[count].weight *= 3;
-				       words[count].weight += str[j];
-				       ++j;
-				}
-				words[count].last = j-1;
-				++count;
+			       words[count].weight *= 3;
+			       words[count].weight += str[j] - 40;
+			       ++j;
 			}
+			words[count].last = j-1;
+			++count;
 		}
 	}
 
+	for (int i = count-1; i >= 0; --i)		//sort
+	{
+		for (int j = i; j >= 0; --j)
+			if (words[j].weight > temp.weight)
+			{
+				temp = words[j];
+				temp_count = j;
+			}
+		words[temp_count] = words[i];
+		words[i] = temp;
+		temp.weight = 0;
+	}
 
-	for (int i = 0; i < N/2; ++i)
-//		printf("%8d %8d %20lld\n", words[i].first, words[i].last, words[i].weight);
+	for (int i = 0; i < count; ++i)		//print words
+	{
+		for (int j = words[i].first; j <= words[i].last; ++j)
+			putchar(str[j]);
+		printf("\n");
+	}
 
 	return 0;
 }
