1095067614f69b2f970006de78b48a36a5d1a9e5
nedoletoff <nedoletoff@yandex.ru>
2020-11-03 17:56:52 +0300

    translate comments to russian

---
+++ badidea.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#define N 128
+
+struct word
+       	{
+		int first;
+		int len;
+		long long weight;
+	};
+
+int compare(struct word t, struct word c)
+{
+	while (t.len > c.len)
+	{
+		--t.len;
+//		t.weight -= 60;
+		t.weight /= 6;	
+	}	
+	while (c.len > t.len)
+	{
+		--c.len;
+//		c.weight -= 60;
+		c.weight /= 6;	
+	}	
+
+	if (c.weight >= t.weight)
+		return 1;
+	else 
+		return 0;
+}
+
+int main()
+{
+	struct word words[N/2] = {0}; 
+	struct word temp = {0};
+	int temp_count = 0;
+	char str[N] = {'\0'};
+	int num[N] = {0};
+	int count = 0;
+
+	printf("Type string: ");
+	str[0] = getchar();
+	num[0] = str[0];
+	for (int i = 1; i < N - 1; ++i)		//read string
+	{
+		str[i] = getchar();
+		if ( str[i-1] == ' ' || str[i] == ' ') 
+			num[i] = str[i];
+		if (str[i] == '\0' || str[i] == '\n')
+		{
+			num[i] = ' ';
+			break;
+		}
+	}
+
+	for (int i = 'A'; i <= 'z'; ++i)	// add weight and count words
+	for (int j = 0; j < N; ++j) 
+	{
+		if (i == num[j])
+		{
+			words[count].first = j;
+			while (num[j] != ' ')
+			{
+			       words[count].weight *= 6;
+			       words[count].weight += str[j] - 40;
+			       ++j;
+			}
+			words[count].len = j - words[count].first;
+			++count;
+		}
+	}
+
+	for (int i = count-1; i >= 0; --i)		//sort
+	{
+		for (int j = i; j >= 0; --j)
+			if (compare(temp, words[j]))
+			{
+				temp = words[j];
+				temp_count = j;
+			}
+		words[temp_count] = words[i];
+		words[i] = temp;
+		temp.weight = 0;
+	}
+
+	for (int i = 0; i < count; ++i)		//print words
+	{
+		int j = words[i].len;
+		int k = words[i].first;
+		while (j > 0)
+		{
+			putchar(str[k]);
+			++k;
+			--j;
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
--- sort.c
+++ sort.c
@@ -1,90 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define N 128
-
-struct word
-       	{
-		int first;
-		int len;
-		long long weight;
-	};
-
-struct word max_min(struct word a, struct word b)
-{
-	if (a.len > b.len)
-		return a, b;
-	else 
-		return b, a;
-}
-
-int compare(struct word longw, struct word shortw)
-{
-	
-	return 1;
-}
 
 int main()
 {
-	struct word words[N/2] = {0}; 
-	struct word temp = {0};
 	int temp_count = 0;
 	char str[N] = {'\0'};
-	int num[N] = {0};
 	int count = 0;
+	int first = 0;
+	int numw = 0;
+	char** words = NULL;
+	char* temp = NULL;
+	int len = 0;
 
 	printf("Type string: ");
-	str[0] = getchar();
-	num[0] = str[0];
-	for (int i = 1; i < N - 1; ++i)		//read string
+	for (int i = 0; i < N - 1; ++i)		//считать строку и посчитать пробелы
 	{
 		str[i] = getchar();
-		if ( str[i-1] == ' ' || str[i] == ' ') 
-			num[i] = str[i];
+		if (str[i] == ' ')
+		       ++numw;
 		if (str[i] == '\0' || str[i] == '\n')
 		{
-			num[i] = ' ';
+			str[i] = ' ';
+			++numw;
 			break;
 		}
 	}
+	
+	words = (char**) malloc(numw * sizeof(char*));
 
-	for (int i = 'A'; i <= 'z'; ++i)	// add weight and count words
-	for (int j = 0; j < N; ++j) 
+	for (int j = 0; j < N; ++j)		//посчитать количество слов и создать слова
 	{
-		if (i == num[j])
+		if (str[j] != ' ' && str[j] != '\0')
 		{
-			words[count].first = j;
-			while (num[j] != ' ')
+			len = 0;
+			first = j;
+			while (str[j] != ' ' && str[j] != '\0')
 			{
-			       words[count].weight *= 3;
-			       words[count].weight += str[j] - 40;
-			       ++j;
+				++len;
+				++j;
 			}
-			words[count].len = j - words[count].first;
+			words[count] = (char*) malloc((len + 1) * sizeof(char));
+			for (int i = 0; i <= len; ++i)
+			{
+				words[count][i] = str[first];
+				++first;
+			}
+			words[count][len++] = '\0';
 			++count;
 		}
 	}
+	for (int i = count; i <= numw; ++i)	//удалить лишние адреса
+		free(words[i]);
 
-	for (int i = count-1; i >= 0; --i)		//sort
-	{
-		for (int j = i; j >= 0; --j)
-			if (words[j].weight > temp.weight )
-			{
-				temp = words[j];
-				temp_count = j;
-			}
-		words[temp_count] = words[i];
-		words[i] = temp;
-		temp.weight = 0;
-	}
-
-	for (int i = 0; i < count; ++i)		//print words
-	{
-		int j = words[i].len;
-		int k = words[i].first;
-		while (j > 0)
+	for (int i = 0; i < count; ++i)		//упорядочить слова
+	for (int j = i+1; j < count; ++j)
 		{
-			putchar(str[k]);
-			++k;
-			--j;
+			temp_count = 0;
+			while  (words[i][temp_count] != '\0' && words[j][temp_count] != '\0' && words[i][temp_count] == (words[j][temp_count]))
+				++temp_count;
+			if (words[i][temp_count] > words[j][temp_count])
+			{
+				temp = words[i];
+				words[i] = words[j];
+				words[j] = temp;
+			}
+		}
+		
+				
+				
+
+	for (int i = 0; i < count; ++i)		//вывести слова в нужном порядке
+	{
+		int j = 0;
+		while (words[i][j] != '\0')
+		{
+			putchar(words[i][j]);
+//			printf("-%d ", words[i][j]);
+			++j;
 		}
 		printf("\n");
 	}
